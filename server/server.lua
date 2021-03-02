8fdcdee9cc9838c5ffda23a01ee897cbb65bb84c
mrv_gitlab <mrvmail0@gmail.com>
2021-03-02 19:16:43 +0100

    fix(server): indentation

--- server/server.lua
+++ server/server.lua
@@ -3,7 +3,7 @@ local neededGameBuild = 2060
 local currentGameBuild = GetConvarInt('sv_enforceGameBuild', 1604)
 
 Citizen.CreateThread(function()
-	if (currentGameBuild < neededGameBuild) then
-		print('^3['..GetCurrentResourceName()..']^0: You need to use ^3' .. neededGameBuild .. '^0 game build (or above) to use this resource.')
-	end
+    if (currentGameBuild < neededGameBuild) then
+        print('^3['..GetCurrentResourceName()..']^0: You need to use ^3' .. neededGameBuild .. '^0 game build (or above) to use this resource.')
+    end
 end)
